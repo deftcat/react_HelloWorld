0e3c5ad8a5c72ae8f136cbf78d035e2fbb91b428
Fei <ycshao@nlg.csie.ntu.edu.tw>
2017-06-16 16:23:26 +0800

    add change value method

--- src/component/store/Breads.js
+++ src/component/store/Breads.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 export default class Breads extends Component {
+  constructor(){
+    super();
+    this.state = {
+      favorite : 'Balic bread'
+    }
+  }
+  changeFavorite(e){
+    console.log('====================================')
+    console.log(e)
+    console.log('====================================')
+    this.setState({favorite: e.target.value})
+  }
   render() {
     return (
       <div>
         <p>This is breads</p>
+        <input type='text' onChange={this.changeFavorite.bind(this)}/>
+        {/*<Favorite />*/}
+        <mark>{this.state.favorite}</mark>
       </div>
     )
   }
 }
+const Favorite = () => (
+  <input type='text' onChange={this.changeFavorite.bind(this)}/>
+);
+
