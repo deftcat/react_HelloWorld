0d601fb6e866088c46a1295605fc3eeb397d48bf
Fei <ycshao@nlg.csie.ntu.edu.tw>
2017-06-16 17:29:42 +0800

    set a target event

--- src/component/Container.js
+++ src/component/Container.js
@@ -5,11 +5,17 @@ export default class Container extends Component {
         super();
         this.state = {
             title: 'Welcome',
-            subtitle: 'subtitle'
+            subtitle: 'subtitle',
+            currentEvent: '--'
         };
+        this.changeEvent = this.changeEvent.bind(this);
     }
     changeSubtitle(subtitle) {
         this.setState({ subtitle });
+    }
+    changeEvent(e){
+        console.log(e);
+        this.setState({currentEvent: e.type})
     }
 
     render() {
@@ -17,7 +23,9 @@ export default class Container extends Component {
             <div>
                 <h2 >{this.state.title}</h2>
                 <h3>{this.state.subtitle}</h3>
-                <Section changeSubtitle={this.changeSubtitle.bind(this)}/>
+                <Section changeSubtitle={this.changeSubtitle.bind(this)} />
+                <h4>the event : {this.state.currentEvent}</h4>
+                <textarea rows='4' cols='4' onKeyPress={this.changeEvent} />
             </div>
         );
     }
