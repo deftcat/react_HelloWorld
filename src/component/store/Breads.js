0692f02a56728abc54880513b7d77379b2ca4aef
Fei <ycshao@nlg.csie.ntu.edu.tw>
2017-06-16 16:33:26 +0800

    practice two component to bind the change event

--- src/component/store/Breads.js
+++ src/component/store/Breads.js
@@ -1,29 +1,28 @@
 import React, { Component } from 'react';
 export default class Breads extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      favorite : 'Balic bread'
+      favorite: 'Balic bread'
     }
   }
-  changeFavorite(e){
-    console.log('====================================')
-    console.log(e)
-    console.log('====================================')
-    this.setState({favorite: e.target.value})
+  changeFavorite(e) {
+    this.setState({ favorite: e.target.value })
   }
   render() {
     return (
       <div>
-        <p>This is breads</p>
-        <input type='text' onChange={this.changeFavorite.bind(this)}/>
-        {/*<Favorite />*/}
-        <mark>{this.state.favorite}</mark>
+        <p>This is my favorite bread: <mark>{this.state.favorite}</mark></p>
+        <label>Stander input w. same level</label>
+        <input type='text' onChange={this.changeFavorite.bind(this)} />
+        <br />
+        <label>From children and bind the change event</label>
+        <Favorite update={this.changeFavorite.bind(this)} />
       </div>
     )
   }
 }
-const Favorite = () => (
-  <input type='text' onChange={this.changeFavorite.bind(this)}/>
+const Favorite = (props) => (
+  <input type='text' onChange={props.update} />
 );
 
