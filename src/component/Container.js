2f42421d467b97cdc7d2a92184350e7cc1d14d5d
Fei <ycshao@nlg.csie.ntu.edu.tw>
2017-06-23 16:28:23 +0800

    add fetch feature

--- src/component/Container.js
+++ src/component/Container.js
@@ -6,22 +6,29 @@ export default class Container extends Component {
         this.state = {
             title: 'Welcome',
             subtitle: 'subtitle',
-            currentEvent: '--'
+            currentEvent: '--',
+            results: [],
         };
         this.changeEvent = this.changeEvent.bind(this);
+    }
+    componentDidMount() {
+        let user = fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then((results) => { this.setState({ results: results.map((result => result.username)) }) });
+        //console.log(user);    
     }
     changeSubtitle(subtitle) {
         this.setState({ subtitle });
     }
-    changeEvent(e){
-        console.log(e);
-        this.setState({currentEvent: e.type})
+    changeEvent(e) {
+        this.setState({ currentEvent: e.type })
     }
 
     render() {
         return (
             <div>
                 <h2 >{this.state.title}</h2>
+                <p> {this.state.results.map(result => <li key={result}>{result}</li>)}</p>
                 <h3>{this.state.subtitle}</h3>
                 <Section changeSubtitle={this.changeSubtitle.bind(this)} />
                 <h4>the event : {this.state.currentEvent}</h4>
